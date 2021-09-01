cedaaeb458958579a7a66fa421b81ccfc2c2da0f
Janet Iqal <j.iqal35@gmail.com>
2021-09-01 13:57:38 -0500

    error w duplicaiton

--- models/index.js
+++ models/index.js
@@ -2,23 +2,21 @@ const Traveller = require('./Travellers');
 const Location = require('./Locations');
 const Trip = require('./Trips');
 
-// Trip.hasMany(Traveller, {
-//     foreignKey: 'traveller_id',
-// })
-
-// Trip.hasOne(Location, {
-//     foreignKey: 'location_id'
-// })
 
 Traveller.belongsToMany(Location, {
     through: Trip,
-    foreignKey: 'traveller_id',
+    foreignKey: 'traveller_id'
+},
+{
     unique: false
 })
 
 Location.belongsToMany(Traveller,{
     through: Trip,
-    foreignKey: 'location_id',
+    foreignKey: 'location_id'
+   
+},
+{
     unique: false
 })
 
